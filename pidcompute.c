3d54fd364f08f978450865d033eb61f21eebb8c0
Hayden Hellsten <haydenhellsten@icloud.com>
2026-03-17 00:18:15 -0400

    Initial commit (pre Wasm module implementation)

---
+++ pidcompute.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <emscripten/emscripten.h>
+
+// Signal buffer format: First value is the initial signal; all following values correspond to indices in the indices buffer
+// Indices buffer format: First value is the total length of the simulation; all following values correspond to values in the indices buffer
+EMSCRIPTEN_KEEPALIVE
+void pid_compute(double* signals, double initialMeasurement, uint16_t* indices, double* output, double kP, double kI, double kD, double dt, double maxOutput, double kVoltage, uint8_t kMeasurement) {
+    const double kdProd = kD/dt;
+    const double kiProd = kI*dt;
+    if (kMeasurement) {
+        double v = initialMeasurement;
+        double prevError = *signals - v;
+        double integral = prevError;
+        double a = kVoltage*12*(kP*prevError + kiProd*integral);
+        v += a*dt;
+        uint16_t length = *(indices++);
+        for (uint16_t current = 0; current < length; current++) {
+            if (current == *indices) {
+                signals++;
+                indices++;
+            }
+            double error = *signals - v;
+            integral += error;
+            a = (kP*error + kiProd*integral + kdProd*(error-prevError));
+            if (a > maxOutput)
+                a = maxOutput;
+            else if (a < -maxOutput)
+                a = -maxOutput;
+            a *= kVoltage*12;
+            v += a*dt;
+            *output = v;
+            output++;
+            prevError = error;
+        }
+    } else {
+        double x = initialMeasurement;
+        double prevError = *signals - x;
+        double integral = prevError;
+        double a = kVoltage*12*(kP*prevError + kiProd*integral);
+        double v = a*dt;
+        x += v*dt;
+        uint16_t length = *(indices++);
+        for (uint16_t current = 0; current < length; current++) {
+            if (current == *indices) {
+                signals++;
+                indices++;
+            }
+            double error = *signals - x;
+            integral += error;
+            a = (kP*error + kiProd*integral + kdProd*(error-prevError));
+            if (a > maxOutput)
+                a = maxOutput;
+            else if (a < -maxOutput)
+                a = -maxOutput;
+            a *= kVoltage*12;
+            v += a*dt;
+            x += v*dt;
+            *output = x;
+            output++;
+            prevError = error;
+        }
+    }
+}
